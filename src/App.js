21d21e0f1ffaa59a07475107ee197eaf7a99a46f
Harshit Gupta <harshitsaamu@gmail.com>
2020-08-15 14:27:18 +0530

    add react files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Card from "@material-ui/core/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            languages: [],
+            selectedLanguage: '',
+            isLoading: false,
+            helloWorldText: 'Hello World'
+        };
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+        fetch('https://cors-anywhere.herokuapp.com/' + 'https://thawing-ocean-61652.herokuapp.com/languages')
+            .then(response => response.json())
+            .then(data => this.setState({languages: data, isLoading: false}));
+    }
+
+    handleChange = (event) => {
+        this.setState({selectedLanguage: event.target.value});
+        console.log(event.target.value);
+        this.fetchData(event.target.value);
+    };
+
+    fetchData(language) {
+        fetch('https://cors-anywhere.herokuapp.com/' + `https://thawing-ocean-61652.herokuapp.com/hello?language=${language}`)
+            .then(response => response.json())
+            .then(data => this.setState({helloWorldText: data, isLoading: false}));
+    }
+
+    render() {
+        return (
+            <div><Card style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: 'column'
+            }}>
+                Hello World App
+                <br/>
+                <br/>
+                <FormControl style={{width: 100}}>
+                    <InputLabel id="demo-simple-select-label">Language</InputLabel>
+                    <Select style={{width: 100}}
+                            labelId="select-label"
+                            id="demo-simple-select"
+                            value={this.state.selectedLanguage}
+                            onChange={this.handleChange}
+                    >
+                        {this.state.languages.map((value) => (
+                            <MenuItem value={value}>{value}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                <br/>
+                {this.state.helloWorldText}
+            </Card>
+            </div>)
+    }
 }
 
 export default App;
