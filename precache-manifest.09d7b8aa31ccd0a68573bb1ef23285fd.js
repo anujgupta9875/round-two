7b5c87d28f4ed9f278e47b5b6dbb87243d426e2d
anujgupta.ag07@gmail.com <harshitsaamu@gmail.com>
2020-08-15 14:50:40 +0530

    Updates

---
+++ precache-manifest.09d7b8aa31ccd0a68573bb1ef23285fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ccfc512cbef2f701fb7142df1c5e4a",
+    "url": "/round-two/index.html"
+  },
+  {
+    "revision": "48312493e321bc55aef9",
+    "url": "/round-two/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f353568a71cf2580103b",
+    "url": "/round-two/static/js/2.599dbd88.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/round-two/static/js/2.599dbd88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48312493e321bc55aef9",
+    "url": "/round-two/static/js/main.679afe30.chunk.js"
+  },
+  {
+    "revision": "d8d13fb4376db3c969aa",
+    "url": "/round-two/static/js/runtime-main.2c7babb9.js"
+  }
+]);
